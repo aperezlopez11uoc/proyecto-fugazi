6976d537e7b30daf017c5b80ce3432f0e1d1d275
root <root@DVPS.myguest.virtualbox.org>
2026-03-23 21:01:17 +0100

    primera version

---
+++ app/go.mod
@@ -0,0 +1,3 @@
+module devops-uoc
+
+go 1.26.1
---
+++ app/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"net/http"
+	"os"
+)
+
+func main() {
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+
+	fs := http.FileServer(http.Dir("static"))
+	http.Handle("/static/", http.StripPrefix("/static/", fs))
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		http.ServeFile(w, r, "static/index.html")
+	})
+
+	http.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("OK"))
+	})
+
+	http.ListenAndServe(":"+port, nil)
+}
